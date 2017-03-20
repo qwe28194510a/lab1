de790ce19734cc7bbb760124112c42091c7cd0a2
pd2vm <qwe28194510a>
2017-03-20 19:10:16 +0800

    lab1 homework

---
+++ lab1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Please input a number:";
+cin>>n;
+cout<<n<<" ";
+
+while(n!=1){
+
+if(n%2==1)
+{n=3*n+1;}
+
+else
+{n=n/2;}
+cout<<n<<" ";
+
+}
+cout<<endl;
+return 0;}
